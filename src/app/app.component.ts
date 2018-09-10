b446c7f7ee1ddc13b7192e5464d4d8a4e8484cba
Gabor Thury <thury.gabor1994@gmail.com>
2018-09-10 20:57:50 +0200

    Routing works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template: `
-  <app-header> </app-header>
-  <app-footer> </app-footer>
-  `
 })
 export class AppComponent {
   title = 'photography-site';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,33 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { GalleriesComponent } from './components/galleries/galleries.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MyPhotosComponent } from './components/my-photos/my-photos.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { GroupsComponent } from './components/groups/groups.component';
+
+const paths: Routes = [
+  { path: '', component: MyPhotosComponent },
+  { path: 'galleries', component: GalleriesComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'groups', component: GroupsComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    GalleriesComponent,
+    MyPhotosComponent,
+    ProfileComponent,
+    GroupsComponent,
   ],
   imports: [
     BrowserModule,
-    TooltipModule.forRoot()
+    TooltipModule.forRoot(),
+    RouterModule.forRoot(paths)
   ],
   providers: [],
   bootstrap: [AppComponent]
