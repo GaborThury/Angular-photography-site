2fa6ea825c687472d3e6a41517e5625969403e56
Gabor Thury <thury.gabor1994@gmail.com>
2018-09-18 10:45:57 +0200

    footer with social media buttons, photo upload is now connected with DB

--- src/app/components/photo-upload/photo-upload.component.ts
+++ src/app/components/photo-upload/photo-upload.component.ts
@@ -10,17 +10,22 @@ export class PhotoUploadComponent implements OnInit {
   selectedFile: File = null;
   onFileSelected(event) {
     this.selectedFile = <File>event.target.files[0];
+    console.log('On file selected.');
+    console.log(this.selectedFile);
   }
 
   onUpload() {
     const fd = new FormData();
-    fd.append('image', this.selectedFile, this.selectedFile.name);
-      this.http.post('PASTE URL HERE', fd);
+    fd.append('photo', this.selectedFile, this.selectedFile.name);
+    this.http.post('http://localhost:3010/photos', fd)
+    .subscribe(d => {
+      console.log(d);
+    });
+    console.log('upload');
   }
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
-
 }
